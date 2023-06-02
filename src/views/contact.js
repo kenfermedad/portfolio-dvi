efcaa3082cf7f7bf8efb5f4135b70bf15e87b661
Cesar <cesarlaresrosas@gmail.com>
2023-06-02 14:23:01 -0300

    segundo
    segundo

--- src/views/contact.js
+++ src/views/contact.js
@@ -4,15 +4,15 @@ export default function Contact() {
     return (
         <div className="presentation row align-items-center">
             <div className="col-sm-12 col-md-6 col-lg-6 info">
-                <h1 className="display-4 info-h1">Daniel Velásquez Iturrate</h1>
+                <h1 className="display-4 info-h1">Cesar Lares</h1>
                 <h3 className="info-h3">Contact Me!</h3>
                 <p>If you are interested in hiring my services, contact me at any of these ways:</p>
             </div>
             <div className="col-sm-12 col-md-6 col-lg-6 contact">
-                <div><i className="fa fa-phone-square" aria-hidden="true"></i>+34654288465</div>
-                <div><i className="fa fa-envelope" aria-hidden="true"></i>velasqueziturrate@gmail.com</div>
-                <div><i className="fab fa-linkedin" /><a href="https://www.linkedin.com/in/daniel-velasquez-iturrate" target="_blank" rel="noopener noreferrer"> /daniel-velasquez-iturrate</a></div>
-                <div><i class="fas fa-map-marker-alt" aria-hidden="true"></i>Nuestra Señora del Villar, 9, Madrid, España 28017</div>
+                <div><i className="fa fa-phone-square" aria-hidden="true"></i>+5491124035669</div>
+                <div><i className="fa fa-envelope" aria-hidden="true"></i>cesarlaresrosas@gmail.com</div>
+                <div><i className="fab fa-linkedin" /><a href="https://www.linkedin.com/in/cesar-lares-48802714a/" target="_blank" rel="noopener noreferrer"> /cesar-lares</a></div>
+                <div><i class="fas fa-map-marker-alt" aria-hidden="true"></i>Av Rivadavia 2982, CABA, Argentina</div>
             </div>
         </div>
     );
--- src/views/resume.js
+++ src/views/resume.js
@@ -9,26 +9,26 @@ export default function Resume() {
             <div className="row resume">
                 <div className="col-md-12 col-lg-4 resume-left-col" >
                     <div className="text-center">
-                        <img className="avatar-img-resume" src="https://firebasestorage.googleapis.com/v0/b/portfolio-daniel-velasquez.appspot.com/o/img%2FAvatarResume.jpg?alt=media&token=c61e2763-6bca-4abb-9af8-275924484328" alt="Avatar Daniel Velásquez Iturrate" />
+                        <img className="avatar-img-resume" src="https://firebasestorage.googleapis.com/v0/b/portafolio-cdeff.appspot.com/o/image_6483441.JPG?alt=media&token=d9a89da6-106d-4d4a-ae7f-f2b419399358&_gl=1*105k5og*_ga*NTAxNjQ1MDE5LjE2ODU1NTM3NzI.*_ga_CW55HF8NVT*MTY4NTU1ODM4Mi4yLjEuMTY4NTU1ODM5Mi4wLjAuMA.." alt="Avatar Daniel Velásquez Iturrate" />
                     </div>
-                    <h2 className="resume-h2">Daniel Velásquez Iturrate</h2>
+                    <h2 className="resume-h2">Cesar Lares</h2>
                     <h4 className="sub-resume">Software Developer</h4>
                     <hr className="hr-resume-left" />
                     <p>I am a Systems Engineer with knowledge in software development, especially in the Front End area. In addition, I am interested in learning about Data Science, AWS, IA and Google Cloud.</p>
                     <hr className="hr-resume-left" />
                     <h5>Address</h5>
-                    <p>Nuestra Señora del Villar, 9, Madrid,España 28017</p>
+                    <p>Av Rivadavia, 2982, CABA, Argentina </p>
                     <h5>Phone</h5>
-                    <p>+34654288465</p>
+                    <p>+54-9-1124035669</p>
                     <h5>Email</h5>
-                    <p>velasqueziturrate@gmail.com</p>
+                    <p>cesarlaresrosas@gmail.com</p>
                     {/* <h5>Web</h5>
                     <p>mywebsite.com</p> */}
                     <hr className="hr-resume-left" />
                     <h5>Attitudes</h5>
                     <ul>
                         <li>
-                        Intermediate level of English
+                        Advanced level of English
                         </li>
                         <li>
                         Advanced level in Microsoft tools
@@ -40,14 +40,14 @@ export default function Resume() {
                     <hr className="hr-resume-left" />
                     <h5>Download Curriculum Vitae</h5>
                     <div className="row">
-                    <div className="col-md-12 col-lg-6 down"><a href="https://firebasestorage.googleapis.com/v0/b/portfolio-daniel-velasquez.appspot.com/o/doc%2FSD%20-%20Daniel%20Vel%C3%A1squez%20Iturrate%20-%20CV%20English.pdf?alt=media&token=2dad344b-1004-42ff-94f0-9be600f1377d" rel="noopener noreferrer" target="_blank" alt="Download English">English <i className="fas fa-download"/></a></div>
-                    <div className="col-md-12 col-lg-6 down"><a href="https://firebasestorage.googleapis.com/v0/b/portfolio-daniel-velasquez.appspot.com/o/doc%2FSD%20-%20Daniel%20Vel%C3%A1squez%20Iturrate%20-%20CV%20Espa%C3%B1ol.pdf?alt=media&token=b16d8976-c53d-4069-a246-f1afa601ef1f" rel="noopener noreferrer" target="_blank" alt="Download Español">Español <i className="fas fa-download" /></a></div>
+                    <div className="col-md-12 col-lg-6 down"><a href="https://firebasestorage.googleapis.com/v0/b/portafolio-cdeff.appspot.com/o/CV.pdf?alt=media&token=fa515a53-8be0-4ee2-8780-7e5b6606e4a6&_gl=1*zq3uoy*_ga*NTAxNjQ1MDE5LjE2ODU1NTM3NzI.*_ga_CW55HF8NVT*MTY4NTU1Mzc3MS4xLjEuMTY4NTU1NjEyMC4wLjAuMA.." rel="noopener noreferrer" target="_blank" alt="Download English">English <i className="fas fa-download"/></a></div>
+                    <div className="col-md-12 col-lg-6 down"><a href="https://firebasestorage.googleapis.com/v0/b/portafolio-cdeff.appspot.com/o/CV.pdf?alt=media&token=fa515a53-8be0-4ee2-8780-7e5b6606e4a6&_gl=1*zq3uoy*_ga*NTAxNjQ1MDE5LjE2ODU1NTM3NzI.*_ga_CW55HF8NVT*MTY4NTU1Mzc3MS4xLjEuMTY4NTU1NjEyMC4wLjAuMA.." rel="noopener noreferrer" target="_blank" alt="Download Español">Español <i className="fas fa-download" /></a></div>
                     </div>
                     <hr className="hr-resume-left" />
                     <h5>Download Cover Letter</h5>
                     <div className="row">
-                    <div className="col-md-12 col-lg-6 down"><a href="https://firebasestorage.googleapis.com/v0/b/portfolio-daniel-velasquez.appspot.com/o/doc%2FSD%20-%20Cover%20Letter%20General.pdf?alt=media&token=0e57e1f6-cdb4-4db1-8d6c-ac06923ee18e" rel="noopener noreferrer" target="_blank" alt="Download English">English <i className="fas fa-download"/></a></div>
-                    <div className="col-md-12 col-lg-6 down"><a href="https://firebasestorage.googleapis.com/v0/b/portfolio-daniel-velasquez.appspot.com/o/doc%2FSD%20-%20Carta%20de%20Presentaci%C3%B3n%20General.pdf?alt=media&token=b9c9ec4d-c254-4928-bf38-1c59b28af1f9" rel="noopener noreferrer" target="_blank" alt="Download Español">Español <i className="fas fa-download" /></a></div>
+                    <div className="col-md-12 col-lg-6 down"><a href="https://firebasestorage.googleapis.com/v0/b/portafolio-cdeff.appspot.com/o/Carta%20de%20presentación%20-%20Cesar%20Andres%20Lares%20Rosas.pdf?alt=media&token=36a59b56-14d9-4daa-9c11-e551d1799b44&_gl=1*8ero5l*_ga*NTAxNjQ1MDE5LjE2ODU1NTM3NzI.*_ga_CW55HF8NVT*MTY4NTU1Mzc3MS4xLjEuMTY4NTU1NjQxNC4wLjAuMA.." rel="noopener noreferrer" target="_blank" alt="Download English">English <i className="fas fa-download"/></a></div>
+                    <div className="col-md-12 col-lg-6 down"><a href="https://firebasestorage.googleapis.com/v0/b/portafolio-cdeff.appspot.com/o/Carta%20de%20presentación%20-%20Cesar%20Andres%20Lares%20Rosas.pdf?alt=media&token=36a59b56-14d9-4daa-9c11-e551d1799b44&_gl=1*8ero5l*_ga*NTAxNjQ1MDE5LjE2ODU1NTM3NzI.*_ga_CW55HF8NVT*MTY4NTU1Mzc3MS4xLjEuMTY4NTU1NjQxNC4wLjAuMA.." rel="noopener noreferrer" target="_blank" alt="Download Español">Español <i className="fas fa-download" /></a></div>
                     </div>
                     <hr className="hr-resume-left" />
                     {/* Link LinkedIn */}
@@ -63,7 +63,7 @@ export default function Resume() {
                         schoolName="Unidad Educativa Colegio San Agustín El Marqués"
                         schoolDescription="High School Diploma with a focus on Science"
                         startDateUniversity="Sep 2015"
-                        endDateUniversity="Mar 2020"
+                        endDateUniversity="Mar 2019"
                         universityName="Universidad Metropolitana"
                         universityDescription="Systems Engineer Degree"
                     />
